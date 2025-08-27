3ea49602d76f785df8d70429c4b18c590c1de64e
pham xuan nam <xuannampham2001@gmail.com>
2025-08-27 19:34:41 +0700

    setup prj

---
+++ src/main/java/com/phmxnnam/prj_banking/PrjBankingApplication.java
@@ -0,0 +1,13 @@
+package com.phmxnnam.prj_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjBankingApplication.class, args);
+	}
+
+}
